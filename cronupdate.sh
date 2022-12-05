5dd6d2de475f1b5b75d74f912197fb8dc5e8a8a7
Sissines <92338885+Sissines@users.noreply.github.com>
2022-12-05 12:50:44 -0500

    Update cronupdate.sh

--- cronupdate.sh
+++ cronupdate.sh
@@ -4,7 +4,7 @@ echo "deb http://security.debian.org/debian-security bullseye-security main" >> 
 echo "deb-src http://security.debian.org/debian-security bullseye-security main" >> /etc/apt/security.sources.list
 mkdir /root/script
 touch /root/script/update.sh
-echo "! /bin/bash" >> /root/script/update.sh
+echo "#! /bin/bash" >> /root/script/update.sh
 echo "sudo apt update -y" >> /root/script/update.sh
 echo "sudo apt-get upgrade -o Dir::Etc::SourceList=/etc/apt/security.sources.list -y" >> /root/script/update.sh
 chmod +x /root/script/update.sh
