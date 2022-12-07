c9295f0a965bc903b0de0a14d6951b5199c59f35
Sissines <92338885+Sissines@users.noreply.github.com>
2022-12-07 12:35:10 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 touch /etc/apt/security.sources.list
 echo "deb http://security.debian.org/debian-security bullseye-security main" >> /etc/apt/security.sources.list
 echo "deb-src http://security.debian.org/debian-security bullseye-security main" >> /etc/apt/security.sources.list
-chmod +x /root/update/update.sh
+chmod +x /update/update.sh
 echo "0 0 15 * * root /root/script/update.sh" >> /etc/crontab
