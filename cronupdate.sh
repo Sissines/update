ba872f83852b75e358b8e6af20e6155688cbee1a
Sissines <92338885+Sissines@users.noreply.github.com>
2022-12-07 11:04:37 -0500

    Update cronupdate.sh

--- cronupdate.sh
+++ cronupdate.sh
@@ -5,7 +5,7 @@ echo "deb-src http://security.debian.org/debian-security bullseye-security main"
 mkdir /root/script
 touch /root/script/update.sh
 echo "#! /bin/bash" >> /root/script/update.sh
-echo "apt update -y | awk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' > test1"$(date +%B)".txt" >> /root/script/update.sh
-echo "apt-get upgrade -o Dir::Etc::SourceList=/etc/apt/security.sources.list -y | awk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' > test1"$(date +%B)".txt" >> /root/script/update.sh
+echo "apt update -y | awk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' > update-"$(date +%H:%M:%S)".log" >> /root/script/update.sh
+echo "apt-get upgrade -o Dir::Etc::SourceList=/etc/apt/security.sources.list -y | awk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' > update-"$(date +%H:%M:%S)".log" >> /root/script/update.sh
 chmod +x /root/script/update.sh
-echo "0 0 15 * * root /root/script/update.sh" >> /etc/crontab
+echo "30 * * * * root /root/script/update.sh" >> /etc/crontab
