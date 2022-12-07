6a5ccaf28690f3f347b9047a9967096ec8b3da39
Sissines <92338885+Sissines@users.noreply.github.com>
2022-12-07 12:13:47 -0500

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+apt update -y | awk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' > "/root/update/logs/update-"$(date +%B)".log"
+apt upgrade -y -o Dir::Etc::SourceList=/etc/apt/security.sources.list | awk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' >> "/root/update/logs/update-"$(date +%B)".log"
